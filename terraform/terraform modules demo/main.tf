e72f7f0fb8f8e29e5960d1b0170440282d2e75e5
ravishansenevirathna <travishan55@gmail.com>
2025-01-25 19:50:51 +0530

    terraform modules

---
+++ terraform/terraform modules demo/main.tf
@@ -0,0 +1,24 @@
+variable "ami_value" {
+    description = "value for the ami"
+}
+
+variable "instance_type_value" {
+    description = "value for the instance type"
+}
+
+# if there is any created subnet use this
+# variable "subnet_value" {
+#     description = "value for the subnet"
+# }
+
+provider "aws" {
+  region = "us-east-1" 
+}
+
+resource "aws_instance" "my-instance" {
+    ami = var.ami_value
+    instance_type = var.instance_type_value
+    # subnet_id = 
+    # key_name = "my-key-pair"
+  
+}
